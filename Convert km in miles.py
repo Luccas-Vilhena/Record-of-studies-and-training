bddc16cf1aa9ecb791ef85d1f2cf3cc79c6ab31c
Luccas <lucas7.lvilhena@gmail.com>
2026-08-25 18:38:00 -0300

    Add files via upload
    simple system to convert distance km/miles

---
+++ Convert km in miles.py
@@ -0,0 +1,6 @@
+print('-'*30)
+print('Km/miles converter'.center(30))
+print('-'*30)
+km = float(input('Distance in km: '))
+milhas = km * 0.6214
+print(f'The distance {km:.2f} km converted to miles is {milhas:.2f} miles')
